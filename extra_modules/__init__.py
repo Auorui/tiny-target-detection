5568699a70b3a76c391be4337d6919ecce8c1c30
夏天是冰红茶 <100789256+Auorui@users.noreply.github.com>
2025-10-27 21:38:21 +0800

    Delete extra_modules directory

--- extra_modules/__init__.py
+++
@@ -1,7 +0,0 @@
-from .norm_block import ConvNormAct, ConvWithoutBN, LayerNorm
-from .ffcayolo import FEM, FFMConcat, SCAM
-from .fbrtyolo import FCM, FBRTDown, MKP
-from .ludyolo import ASFF3, BiLevelRoutingAttention, C2fBRA
-from .vrfdetr import C2fGMCF, TransformerEncoderLayerMSCF
-from .ifyolo import IPFA, CSFM
-from .prnet import ESSamp
