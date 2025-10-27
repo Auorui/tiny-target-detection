91cde0dbb154a1c93ed31a0e53c61accb6c66c05
夏天是冰红茶 <100789256+Auorui@users.noreply.github.com>
2025-10-27 21:39:42 +0800

    Add files via upload

---
+++ ultralytics-tiny/extra_modules/vrfdetr.py
@@ -0,0 +1,188 @@
+import torch
+import torch.nn as nn
+from timm.layers import DropPath
+from einops import rearrange, reduce
+from ultralytics.nn.modules.conv import Conv, DWConv
+from ultralytics.nn.modules.block import C2f
+from ultralytics.nn.extra_modules.norm_block import LayerNorm
+
+class GConv(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.) -> None:
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        hidden_features = int(2 * hidden_features / 3)
+        self.fc1 = nn.Conv2d(in_features, hidden_features * 2, 1)
+        self.dwconv = nn.Sequential(
+            nn.Conv2d(hidden_features, hidden_features, kernel_size=3, stride=1, padding=1, bias=True,
+                      groups=hidden_features),
+            act_layer()
+        )
+        self.fc2 = nn.Conv2d(hidden_features, out_features, 1)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x_shortcut = x
+        x, v = self.fc1(x).chunk(2, dim=1)  # chunk函数用于将张量按指定维度分割成多个等大小或不等大小的块。当元素数量不能整除指定的块数时，最后一个块的大小会较小
+        x = self.dwconv(x) * v
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x_shortcut + x
+
+class MutilScal(nn.Module):
+    def __init__(self, dim=512, fc_ratio=4, dilation=[3, 5, 7], pool_ratio=16):
+        super(MutilScal, self).__init__()
+        self.conv0_1 = Conv(dim, dim//fc_ratio)
+        self.conv0_2 = Conv(dim//fc_ratio, dim//fc_ratio, 3, d=dilation[-3], g=dim//fc_ratio)
+        self.conv0_3 = Conv(dim//fc_ratio, dim, 1)
+
+        self.conv1_2 = Conv(dim//fc_ratio, dim//fc_ratio, 3, d=dilation[-2], g=dim // fc_ratio)
+        self.conv1_3 = Conv(dim//fc_ratio, dim, 1)
+
+        self.conv2_2 = Conv(dim//fc_ratio, dim//fc_ratio, 3, d=dilation[-1], g=dim//fc_ratio)
+        self.conv2_3 = Conv(dim//fc_ratio, dim, 1)
+
+        self.conv3 = Conv(dim, dim, 1)
+
+        self.Avg = nn.AdaptiveAvgPool2d(pool_ratio)
+
+    def forward(self, x):
+        u = x.clone()
+
+        attn0_1 = self.conv0_1(x)
+        attn0_2 = self.conv0_2(attn0_1)
+        attn0_3 = self.conv0_3(attn0_2)
+
+        attn1_2 = self.conv1_2(attn0_1)
+        attn1_3 = self.conv1_3(attn1_2)
+
+        attn2_2 = self.conv2_2(attn0_1)
+        attn2_3 = self.conv2_3(attn2_2)
+
+        attn = attn0_3 + attn1_3 + attn2_3
+        attn = self.conv3(attn)
+        attn = attn * u
+
+        pool = self.Avg(attn)
+
+        return pool
+
+class MSCF(nn.Module):
+    def __init__(self, dim, num_heads=8, atten_drop=0., proj_drop=0., dilation=[3, 5, 7], fc_ratio=4, pool_ratio=16):
+        super(MSCF, self).__init__()
+        assert dim % num_heads == 0, f"dim {dim} should be divided by num_heads {num_heads}."
+        self.dim = dim
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = head_dim ** -0.5
+        self.atten_drop = nn.Dropout(atten_drop)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+        self.MSC = MutilScal(dim=dim, fc_ratio=fc_ratio, dilation=dilation, pool_ratio=pool_ratio)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Conv2d(in_channels=dim, out_channels=dim//fc_ratio, kernel_size=1),
+            nn.ReLU6(),
+            nn.Conv2d(in_channels=dim//fc_ratio, out_channels=dim, kernel_size=1),
+            nn.Sigmoid()
+        )
+        self.kv = Conv(dim, 2 * dim, 1)
+
+    def forward(self, x):
+        u = x.clone()
+        B, C, H, W = x.shape
+        kv = self.MSC(x)
+        kv = self.kv(kv)
+
+        B1, C1, H1, W1 = kv.shape
+
+        q = rearrange(x, 'b (h d) (hh) (ww) -> (b) h (hh ww) d', h=self.num_heads,
+                      d=C // self.num_heads, hh=H, ww=W)
+        k, v = rearrange(kv, 'b (kv h d) (hh) (ww) -> kv (b) h (hh ww) d', h=self.num_heads,
+                         d=C // self.num_heads, hh=H1, ww=W1, kv=2)
+
+        dots = (q @ k.transpose(-2, -1)) * self.scale
+        attn = dots.softmax(dim=-1)
+        attn = self.atten_drop(attn)
+        attn = attn @ v
+
+        attn = rearrange(attn, '(b) h (hh ww) d -> b (h d) (hh) (ww)', h=self.num_heads,
+                         d=C // self.num_heads, hh=H, ww=W)
+        # channel attn
+        c_attn = self.avgpool(x)
+        c_attn = self.fc(c_attn)
+        c_attn = c_attn * u
+        return attn + c_attn
+
+class GMCF(nn.Module):
+    def __init__(self,
+                 inc,
+                 drop_path=0.1,
+                 ):
+        # 用于替换C2f中的Bottleneck
+        super().__init__()
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.mlp = GConv(inc)
+        self.mscfa = nn.Sequential(
+            MSCF(inc),
+            nn.BatchNorm2d(inc)
+        )
+
+    def forward(self, x):
+        x = x + self.drop_path(self.mscfa(x))
+        x = x + self.drop_path(self.mlp(x))
+        return x
+
+class C2fGMCF(C2f):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        super().__init__(c1, c2, n, shortcut, g, e)
+        self.m = nn.ModuleList(GMCF(self.c) for _ in range(n))
+
+
+class TransformerEncoderLayerMSCF(nn.Module):
+    """Defines a single layer of the transformer encoder."""
+    def __init__(self, c1, cm=2048, num_heads=8, dropout=0.0, act=nn.GELU(), normalize_before=False):
+        """Initialize the TransformerEncoderLayer with specified parameters."""
+        super().__init__()
+        self.additivetoken = MSCF(c1, num_heads=num_heads)
+        # Implementation of Feedforward model
+        self.fc1 = nn.Conv2d(c1, cm, 1)
+        self.fc2 = nn.Conv2d(cm, c1, 1)
+
+        self.norm1 = LayerNorm(c1)
+        self.norm2 = LayerNorm(c1)
+        self.dropout = nn.Dropout(dropout)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.act = act
+        self.normalize_before = normalize_before
+
+    def forward_post(self, src, src_mask=None, src_key_padding_mask=None, pos=None):
+        """Performs forward pass with post-normalization."""
+        src2 = self.additivetoken(src)
+        src = src + self.dropout1(src2)
+        src = self.norm1(src)
+        src2 = self.fc2(self.dropout(self.act(self.fc1(src))))
+        src = src + self.dropout2(src2)
+        return self.norm2(src)
+
+    def forward(self, src, src_mask=None, src_key_padding_mask=None, pos=None):
+        """Forward propagates the input through the encoder module."""
+        return self.forward_post(src, src_mask, src_key_padding_mask, pos)
+
+
+if __name__ == "__main__":
+    P2 = torch.randn(1, 128, 80, 80)
+    # P3 = torch.randn(1, 256, 80, 80)
+    # P4 = torch.randn(1, 512, 40, 40)
+    msc = MutilScal(dim=128)
+    y = msc(P2)
+    print("MutilScal Output shape:", y.shape)
+    mscf = MSCF(dim=128)
+    y = mscf(P2)
+    print("MSCF Output shape:", y.shape)
+
+
+
