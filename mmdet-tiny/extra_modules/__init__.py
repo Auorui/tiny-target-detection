b1089e71375b074ac0bf2b5604c3500ab2e03b47
夏天是冰红茶 <100789256+Auorui@users.noreply.github.com>
2025-11-12 11:08:01 +0800

    Add files via upload

--- mmdet-tiny/extra_modules/__init__.py
+++ mmdet-tiny/extra_modules/__init__.py
@@ -1,3 +1,4 @@
 # https://github.com/open-mmlab/mmcv/blob/3c9d327cc78b02569abbaa0e4925a2f7be7c73d9/docs/zh_cn/get_started/api_reference.md
 # https://blog.csdn.net/m0_53176241/article/details/145117207
 from .fp16_utils import auto_fp16
+from .cnn import autopad, BCHW2BHWC, BHWC2BCHW
---
+++ mmdet-tiny/extra_modules/cnn.py
@@ -0,0 +1,28 @@
+# code from "https://github.com/NUST-Machine-Intelligence-Laboratory/PKINet/blob/main/mmrotate/models/utils/cnn.py"
+import torch.nn as nn
+
+def autopad(kernel_size: int, padding: int = None, dilation: int = 1):
+    assert kernel_size % 2 == 1, 'if use autopad, kernel size must be odd'
+    if dilation > 1:
+        kernel_size = dilation * (kernel_size - 1) + 1
+    if padding is None:
+        padding = kernel_size // 2
+    return padding
+
+
+class BCHW2BHWC(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def forward(x):
+        return x.permute([0, 2, 3, 1])
+
+
+class BHWC2BCHW(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def forward(x):
+        return x.permute([0, 3, 1, 2])
